b0fd6f821ff4dfad164b182c07499e1a6518db19
ryuring <ryuring55@gmail.com>
2012-07-05 15:46:39 +0900

    コメントを整理

--- config/blog_share.php
+++ config/blog_share.php
@@ -1,6 +1,23 @@
 <?php
+/* SVN FILE: $Id$ */
+/**
+ * [BlogShare] 設定ファイル
+ *
+ * PHP version 5
+ *
+ * baserCMS :  Based Website Development Project <http://basercms.net>
+ * Copyright 2008 - 2012, baserCMS Users Community <http://sites.google.com/site/baserusers/>
+ *
+ * @copyright		Copyright 2011 - 2012, Catchup, Inc.
+ * @link			http://www.e-catchup.jp Catchup, Inc.
+ * @package			blog_share.config
+ * @since			Baser v 2.0.0
+ * @version			$Revision$
+ * @modifiedby		$LastChangedBy$
+ * @lastmodified	$Date$
+ * @license			MIT lincense
+ */
 	$config['BlogShare'] = array(
 		'contentId'	=> 1,
 		'dbConfigName' => 'share'
 	);
-?>
--- controllers/components/blog_share_hook.php
+++ controllers/components/blog_share_hook.php
@@ -1,4 +1,22 @@
 <?php
+/* SVN FILE: $Id$ */
+/**
+ * [BlogShare] フックコンポーネント
+ *
+ * PHP version 5
+ *
+ * baserCMS :  Based Website Development Project <http://basercms.net>
+ * Copyright 2008 - 2012, baserCMS Users Community <http://sites.google.com/site/baserusers/>
+ *
+ * @copyright		Copyright 2011 - 2012, Catchup, Inc.
+ * @link			http://www.e-catchup.jp Catchup, Inc.
+ * @package			blog_share.controllers.components
+ * @since			Baser v 2.0.0
+ * @version			$Revision$
+ * @modifiedby		$LastChangedBy$
+ * @lastmodified	$Date$
+ * @license			MIT lincense
+ */
 class BlogShareHookComponent extends Object {
 	
 	var $registerHooks = array('startup');
